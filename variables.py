efbac2c1959127faae8c4d7d6216b990a16443a9
Anand <ajkadakkal@gmail.com>
2019-05-29 12:48:15 +0530

    valiables

---
+++ variables.py
@@ -0,0 +1,42 @@
+# greeting="Hello World"
+# print(greeting)
+
+# greeting2=input("Write a greeting : ")
+# print(greeting2)
+
+# age=40
+# print(age)
+# print(type(age))
+
+# age=input("Enter your age : ")
+# print(type(age))
+
+# new_age=int(age)+20
+# print(new_age)
+print(3**2)
+
+# message="Hello World"
+
+# print(message)
+# # print(message.replace("W","M"))
+# # print(dir(message))
+# # help(message.replace)
+# print(message[1:])
+# print(message[:-1])
+# print(message[-1::-1])
+
+# fruits=["Apple","Orange","Banana",45]
+# print(fruits[1])
+# fruits.reverse()
+# print(fruits)
+
+# cordinate=(45,5)
+# print(cordinate)
+# print(cordinate.count(45))
+# print(len(cordinate))
+
+d={"name":"Anand","age":43}
+# print(d["name"])
+
+for key in d:
+	print(d[key])
